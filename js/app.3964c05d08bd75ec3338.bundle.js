8ffa0afa611febf20044e208b46294a31e2fc472
niiloArction <niiloArction@users.noreply.github.com>
2026-01-05 07:55:12 +0000

    deploy: 2508780240c2578c83384fc94e76ea724bdfed51

---
+++ js/app.3964c05d08bd75ec3338.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[524],{44(e,a,s){const n=s(377),{lightningChart:h,MapTypes:o,Themes:r}=n;h({resourcesBaseUrl:new URL(document.head.baseURI).origin+new URL(document.head.baseURI).pathname+"resources/"}).Map({theme:r[new URLSearchParams(window.location.search).get("theme")||"darkGold"]||void 0,type:o.World})}},e=>{e.O(0,[502],()=>e(e.s=44)),e.O()}]);
